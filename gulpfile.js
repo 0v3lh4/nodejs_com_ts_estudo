7c74b5a92e586c5afae55d9f9d18a6ef95be0dca
Hendrik Araújo Mourão <hendrik.mourao@autotrac.com.br>
2018-07-13 10:46:24 -0300

    Limpa a tela antes do lint:watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp')
 const { spawn } = require('child_process')
 
 gulp.task('lint', (cb) => {
+  process.stdout.write('\u001B[2J\u001B[0;0f')
   const cmd = spawn('yarn', ['lint'], { stdio: 'inherit', shell: true })
   cmd.on('close', () => cb())
 })
